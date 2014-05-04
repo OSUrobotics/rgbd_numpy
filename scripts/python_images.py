007ff57ed020e59ad16723bd0690e05fd382bc56
MatthewRueben <ruebenm@onid.oregonstate.edu>
2014-05-03 23:04:56 -0700

    Added conversion to XYZ points. Two-meter filter now works in XYZ at ~9Hz.

--- scripts/python_images.py
+++ scripts/python_images.py
@@ -3,6 +3,7 @@
 import rospy
 from std_msgs.msg import Empty
 from sensor_msgs.msg import CameraInfo, Image
+from image_geometry import PinholeCameraModel
 import numpy
 import cv
 from cv_bridge import CvBridge
@@ -51,6 +52,23 @@ class ImageGrabber():
         self.array_rgb = numpy.asarray(self.bridge.imgmsg_to_cv(self.image_rgb))
         self.array_depth = numpy.asarray(self.bridge.imgmsg_to_cv(self.image_depth, desired_encoding='16UC1'))
 
+    def convert_to_xyz(self):
+        self.array_xyz = numpy.zeros(self.array_rays.shape)
+        for i in range(self.array_rays.shape[2]): 
+            self.array_xyz[:, :, i] = self.array_depth * self.array_rays[:, :, i]
+        self.array_xyz /= 1000  # convert from millimeters -> meters
+
+    def get_rays(self):
+        model = PinholeCameraModel()
+        model.fromCameraInfo(self.depth_info)
+        self.array_rays = numpy.zeros((self.image_depth.height, self.image_depth.width, 3))
+        
+        for u in range(self.image_depth.height):
+            for v in range(self.image_depth.width):
+                ray = model.projectPixelTo3dRay((u, v))
+                ray_z = [el / ray[2] for el in ray]  # normalize the ray so its Z-component equals 1.0
+                self.array_rays[u, v, :] = ray_z
+
     def publish_rgb(self):
         image_cv = cv.fromarray(self.array_rgb)
         image_msg = self.bridge.cv_to_imgmsg(image_cv, encoding='bgr8')
@@ -67,16 +85,35 @@ if __name__ == '__main__':
               'depth_info' : '/camera/depth_registered/camera_info'}
 
     image_grabber = ImageGrabber(topics)
-
+                                        
+    while image_grabber.need_depth:
+        rospy.sleep(0.01)
+        
+    image_grabber.get_rays()  # make map of 3D rays for each pixel for converting depth values -> XYZ points
+    rospy.loginfo('Made array of all pixel->ray correspondences!')
+        
+                                        
     while not rospy.is_shutdown():
+
         if not image_grabber.need_rgb and not image_grabber.need_depth:
 
             image_grabber.convert_to_numpy()
 
-            # APPLY FILTER HERE
+            """ 
+            # TWO-METER DISTANCE FILTER
             image_grabber.array_depth[numpy.isnan(image_grabber.array_depth)] = 0.0
             mask = numpy.logical_or(image_grabber.array_depth > 2000, image_grabber.array_depth == 0)   # 2000mm = 2m
             for i in range(image_grabber.array_rgb.shape[2]): 
                 image_grabber.array_rgb[:, :, i][mask] = 0
+            """
 
+            # PERSON FILTER (if you stand at image center)
+            # Note: +Y is down, +X is right, +Z is "out" of course
+            image_grabber.convert_to_xyz()
+            image_grabber.array_xyz[numpy.isnan(image_grabber.array_xyz)] = 0.0
+            mask = numpy.logical_or(image_grabber.array_xyz[:, :, 2] > 2.0, image_grabber.array_xyz[:, :, 2] == 0)   # 2000mm = 2m
+            for i in range(image_grabber.array_rgb.shape[2]): 
+                image_grabber.array_rgb[:, :, i][mask] = 0
+            
             image_grabber.publish_rgb()
+

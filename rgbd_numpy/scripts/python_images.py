775b8672c454c7810aeaa55f6d9cec7b260cd45e
MatthewRueben <ruebenm@onid.oregonstate.edu>
2014-06-12 13:11:14 -0700

    Used numpy_msg to simplify my rgb/depth image filter.

--- rgbd_numpy/scripts/python_images.py
+++ rgbd_numpy/scripts/python_images.py
@@ -8,6 +8,7 @@ import numpy
 import cv
 from cv_bridge import CvBridge
 import message_filters
+from rospy.numpy_msg import numpy_msg
 
 import code # FOR TESTING
 
@@ -23,18 +24,18 @@ class ImageGrabber():
         self.need_depth = True
 
         # RGB Subscribers
-        sub_rgb = message_filters.Subscriber(topics['rgb'], Image)
+        sub_rgb = message_filters.Subscriber(topics['rgb'], numpy_msg(Image))
         sub_rgb_info = message_filters.Subscriber(topics['rgb_info'], CameraInfo)
         ts_rgb = message_filters.TimeSynchronizer([sub_rgb, sub_rgb_info], 10)
         ts_rgb.registerCallback(self.rgb_callback)
 
         # Depth Subscribers
-        sub_depth = message_filters.Subscriber(topics['depth'], Image)
+        sub_depth = message_filters.Subscriber(topics['depth'], numpy_msg(Image))
         sub_depth_info = message_filters.Subscriber(topics['depth_info'], CameraInfo)
         ts_depth = message_filters.TimeSynchronizer([sub_depth, sub_depth_info], 10)
         ts_depth.registerCallback(self.depth_callback)
 
-        self.pub_filtered = rospy.Publisher('/camera/rgb/image_color_filtered', Image)
+        self.pub_filtered = rospy.Publisher('/camera/rgb/image_color_filtered', numpy_msg(Image))
 
 
     def rgb_callback(self, image_rgb, rgb_info):
@@ -47,15 +48,10 @@ class ImageGrabber():
         self.depth_info = depth_info
         self.need_depth = False
 
-    def convert_to_numpy(self):
-        self.need_rgb = self.need_depth = True  # reset flags
-        self.array_rgb = numpy.asarray(self.bridge.imgmsg_to_cv(self.image_rgb))
-        self.array_depth = numpy.asarray(self.bridge.imgmsg_to_cv(self.image_depth, desired_encoding='16UC1'))
-
     def convert_to_xyz(self):
         self.array_xyz = numpy.zeros(self.array_rays.shape)
         for i in range(self.array_rays.shape[2]): 
-            self.array_xyz[:, :, i] = self.array_depth * self.array_rays[:, :, i]
+            self.array_xyz[:, :, i] = self.image_depth.data * self.array_rays[:, :, i]
         self.array_xyz /= 1000  # convert from millimeters -> meters
 
     def get_rays(self):
@@ -69,10 +65,6 @@ class ImageGrabber():
                 ray_z = [el / ray[2] for el in ray]  # normalize the ray so its Z-component equals 1.0
                 self.array_rays[u, v, :] = ray_z
 
-    def publish_rgb(self):
-        image_cv = cv.fromarray(self.array_rgb)
-        image_msg = self.bridge.cv_to_imgmsg(image_cv, encoding='bgr8')
-        self.pub_filtered.publish(image_msg)
         
 
 if __name__ == '__main__':
@@ -97,15 +89,13 @@ if __name__ == '__main__':
 
         if not image_grabber.need_rgb and not image_grabber.need_depth:
 
-            image_grabber.convert_to_numpy()
-
-            """ 
+            """
             # FILTER #1
             # TWO-METER DISTANCE FILTER
-            image_grabber.array_depth[numpy.isnan(image_grabber.array_depth)] = 0.0
-            mask = numpy.logical_or(image_grabber.array_depth > 2000, image_grabber.array_depth == 0)   # 2000mm = 2m
-            for i in range(image_grabber.array_rgb.shape[2]): 
-                image_grabber.array_rgb[:, :, i][mask] = 0
+            image_grabber.image_depth.data[numpy.isnan(image_grabber.image_depth.data)] = 0.0
+            mask = numpy.logical_or(image_grabber.image_depth.data > 2000, image_grabber.image_depth.data == 0)   # 2000mm = 2m
+            for i in range(image_grabber.image_rgb.data.shape[2]): 
+                image_grabber.image_rgb.data[:, :, i][mask] = 0
             """
 
             """
@@ -115,8 +105,8 @@ if __name__ == '__main__':
             image_grabber.convert_to_xyz()
             image_grabber.array_xyz[numpy.isnan(image_grabber.array_xyz)] = 0.0  # filter out NaNs
             mask = numpy.logical_or(image_grabber.array_xyz[:, :, 2] > 2.0, image_grabber.array_xyz[:, :, 2] == 0)   # 2000mm = 2m
-            for i in range(image_grabber.array_rgb.shape[2]): 
-                image_grabber.array_rgb[:, :, i][mask] = 0
+            for i in range(image_grabber.image_rgb.data.shape[2]): 
+                image_grabber.image_rgb.data[:, :, i][mask] = 0
             """
 
             # FILTER #3
@@ -129,8 +119,11 @@ if __name__ == '__main__':
             mask_z = numpy.logical_and(image_grabber.array_xyz[:, :, 2] < centerpoint[2] + 0.40, image_grabber.array_xyz[:, :, 2] > centerpoint[2] - 0.40)
             mask_xz = numpy.logical_and(mask_x, mask_z)  # draws a 40cm rectangular prism around the centerpoint
             mask = numpy.logical_or(mask_xz, image_grabber.array_xyz[:, :, 2] == 0)  # get rid of NaNs
-            for i in range(image_grabber.array_rgb.shape[2]): 
-                image_grabber.array_rgb[:, :, i][mask] = 0
+            for i in range(image_grabber.image_rgb.data.shape[2]): 
+                image_grabber.image_rgb.data[:, :, i][mask] = 0
 
-            image_grabber.publish_rgb()
+            image_grabber.pub_filtered.publish(image_grabber.image_rgb)
+
+            image_grabber.need_rgb = True   # reset flags
+            image_grabber.need_depth = True
 
